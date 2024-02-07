25d8bbe22381ebc01449d8ed61539845260e4952
bogdan.gherghina <bogdan.gherghina@erepubliklabs.com>
2024-02-07 13:43:39 +0200

    + test commit

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::process;
 
 mod day1;
 mod day2;
+mod day3;
 mod utils;
 
 
@@ -12,7 +13,7 @@ fn main() {
     let semi_file_path = "src/inputs/day";
     let file_path = format!("{semi_file_path}{day}.txt");
     
-    if let Err(e) = day2::run(file_path) {
+    if let Err(e) = day3::run(file_path) {
         println!("Application error: {e}");
         process::exit(1);
     }
